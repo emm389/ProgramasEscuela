31cbd62fe085e3f1549ddafb804599f6805cc6f3
Emmanuel Gonzalez Barreto <emm389@hotmail.com>
2014-05-30 21:15:04 -0500

    Comentarios actualizados

--- Listas/ListProgram.java
+++ Listas/ListProgram.java
@@ -25,7 +25,7 @@ public class ListProgram {
 		switch (eleccion){
 			case 1:
 				System.out.print ("Ingrese el numero que quiera guardar (presione 0 para salir: \n");
-		
+				
 				do{
 					System.out.print ("Tipo: ");
 					numero = teclado.nextInt();
@@ -46,7 +46,8 @@ public class ListProgram {
 				break;
 			case 2:
 				System.out.print ("Ingrese el numero que quiera guardar (presione 0 para salir: \n");
-		
+				
+				//Aqui ingresamos el numero y nos fijamos si es distinto a 0, si es distinto entonces inserta el nodo al principio
 				do{
 					System.out.print ("Tipo: ");
 					numero = teclado.nextInt();
@@ -57,29 +58,31 @@ public class ListProgram {
 				
 				System.out.println ("\n**********");
 				System.out.print ("La lista es: ");
-
+				//regresamos a header
 				ll.rebobinar();
-
+				//recorremos la lista para imprimirla en pantalla
 				while(ll.siguiente()){
 					System.out.print (ll.obtener() + ", ");
 				}
 				System.out.println ("\n");
 				break;
 			case 3:
+				//capturamos los datos
 				System.out.print ("Ingrese el numero que quiera guardar (presione 0 para salir: \n");
 				System.out.print ("Tipo: ");
 				numero = teclado.nextInt();
 				System.out.print ("ingrese la posicion donde se metera el dato: ");
 				int posicion = teclado.nextInt();
 				
+				//si el numero es distinto a 0 inserta el nodo en la posicion pedida
 				if (numero != 0){
 					ll.insertarEntre(numero, posicion);
 				}
 				System.out.println ("\n**********");
 				System.out.print ("La lista es: ");
-
+				//regresamos a header
 				ll.rebobinar();
-
+				//recorremos la lista para imprimirla en pantalla
 				while(ll.siguiente()){
 					System.out.print (ll.obtener() + ", ");
 				}
@@ -89,13 +92,16 @@ public class ListProgram {
 				System.out.print ("Ingresa el numero que quieres borrar: ");
 				int numeroEliminar = teclado.nextInt();
 				
+				//este metodo regresara a header
 				ll.rebobinar();
+				//este metodo ya incluye la busqueda, asi que solo le pasamos como parametro el numero a eliminar
 				ll.eliminar (numeroEliminar);
 				
+				//volvemos a regresarnos a header
 				ll.rebobinar();
 				
 				System.out.print ("La lista es: ");
-
+				//Recorremos la lista para imprimirla en pantalla
 				while(ll.siguiente()){
 					System.out.print (ll.obtener() + ", ");
 				}
@@ -106,8 +112,11 @@ public class ListProgram {
 				int numeroBuscar = teclado.nextInt();
 				Node buscar;
 				
+				//regresamos a header
 				ll.rebobinar();
 				
+				/*le pasamos el numero ingresado como parametro al metodo buscar y con una estructura selectiva
+				 * imprimimos en la pantalla si fue encontrado el numero o no */
 				if (ll.Buscar(numeroBuscar)){
 					System.out.println ("El numero " + numeroBuscar + " si se encuentra en la lista\n");
 				}else{
